be43536ef1c104113f598612c986adaf927ec13a
Letícia Furtado <leticialuzia@icloud.com>
2021-04-11 18:00:40 -0300

    First commit

---
+++ bytebank/src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val titular = "Lelus"
+    val conta = 1001
+    val agencia = 1
+    var saldo = 0.00
+    saldo += 200
+
+    println("Bem vinde ao ByteBank, $titular!")
+    println("Agência: $agencia")
+    println("Conta: $conta")
+    println("Saldo $saldo")
+}
